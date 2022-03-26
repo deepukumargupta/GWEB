096610fa231de006a7dcc9725abc414e72de40c1
deepukumargupta <deepu855305@gmail.com>
2022-03-26 19:37:11 +0530

    add component and admin site

--- frontend/README.md
+++ frontend/README.md
@@ -4,3 +4,16 @@ To check install
 nx
 create project
 npx create-nx-workspace --preset=angular
+
+# For Frontend run project
+path:- Desktop/CourseProject/GWEB/frontend/webstach
+
+# Generate admin project in frontend 
+nx generate @nrwl/angular:app admin
+
+# Run Admin
+nx serve admin 
+# change Admin panel port with
+nx serve admin --port 4100
+# Run User
+nx serve grabtap
---
+++ frontend/webstach/apps/grabtap/src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,18 @@
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // eslint-disable-next-line @angular-eslint/component-selector
+  selector: 'grabtab-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss']
+})
+export class HomePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
