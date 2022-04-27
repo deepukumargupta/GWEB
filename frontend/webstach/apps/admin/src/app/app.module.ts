30107edfbd0b33190a1441b0e18c3fbbf629e5f9
deepukumargupta <deepu855305@gmail.com>
2022-04-28 00:29:51 +0530

    users start

--- frontend/webstach/apps/admin/src/app/app.module.ts
+++ frontend/webstach/apps/admin/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
